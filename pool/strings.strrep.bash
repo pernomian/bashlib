53d440942df5433cd2ebef7d6d6e255a61fbec61
Paulo Pernomian <paulopernomian@gmail.com>
2012-12-08 17:58:39 -0200

    Refactored function.

--- pool/strings.strrep.bash
+++ pool/strings.strrep.bash
@@ -14,16 +14,6 @@ substr2="$2"
 max_rep="$3"
 str="$4"
 
-len_str=$(strlen "$str")
-if [ $len_str -eq 0 ]; then
-	return 2
-fi
-
-len_substr1=$(strlen "$substr1")
-if [ $len_substr1 -gt $len_str ]; then
-	return 2
-fi
-
 if ! $(isInteger "$max_rep"); then
 	return 2
 fi
@@ -31,43 +21,17 @@ if [ $max_rep -lt 0 ]; then
 	return 2
 fi
 
-p2=0
-c=0
-nrep=0
 newstr="$str"
-while [ $p2 -lt $len_str ]; do
-	p1=$(($c + 1))
-	p2=$(($p1 + $len_substr1 - 1))
-	test_sub="$(substring $p1 $p2 "$str")"
-	if [ "$test_sub" == "$substr1" ]; then
-		prev_p1=1
-		prev_p2=$(($p1 - 1))
-		next_p1=$(($p2 + 1))
-		next_p2=$len_str
-		
-		if [ $prev_p2 -lt $prev_p1 ]; then
-			prev_sub=""
-		else
-			prev_sub="$(substring $prev_p1 $prev_p2 "$newstr")"
-		fi
-		
-		if [ $next_p1 -gt $next_p2 ]; then
-			next_sub=""
-		else
-			next_sub="$(substring $next_p1 $next_p2 "$newstr")"
-		fi
-		
-		newstr="$prev_sub""$substr2""$next_sub"
-		
+
+if [ $max_rep -eq 0 ]; then
+	newstr="$(sed "s/$substr1/$substr2/g" <<< "$newstr")"
+else
+	nrep=0
+	while [ $nrep -lt $max_rep ]; do
+		newstr="$(sed "s/$substr1/$substr2/1" <<< "$newstr")"
 		nrep=$(($nrep + 1))
-	fi
-	
-	if [ $max_rep -ne 0 -a $nrep -eq $max_rep ]; then
-		break
-	fi
-	
-	c=$(($c + 1))
-done
+	done
+fi
 
 echo "$newstr"
 
