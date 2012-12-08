7cb8e47c44e6925023598ba2e44af2f84d86f5de
Paulo Pernomian <paulopernomian@gmail.com>
2012-12-08 21:04:07 -0200

    Fixed wrong output (removed single quotes from resulting string).

--- pool/strings.strjoin.bash
+++ pool/strings.strjoin.bash
@@ -9,11 +9,21 @@ fi
 
 str=""
 for i in $(sequence "1-$#"); do
-	str="$str""$1"
+	str="$str$1"
 	shift
 done
 
-echo "$str"
+echo "$(tr -d "\'" <<< "$str")"
+# NOTE: The use of "tr -d" removes the "'" char from the output string.
+#    It happens when passing the output of another function as argument         
+# containing single quotes.
+#    It's unclear the reason for this behavior.
+#    If you try:
+#       strjoin $(strsplit "abcd" "")
+#  it would output "'a''b''c''d'" without this workaround, instead of "abcd".
+#    strsplit function returns characters enclosured by single quotes.
+#    But, hey! If you know the reason and/or the proper fix, and if you would   
+# like to help, please keep in contact.
 
 return 0
 
